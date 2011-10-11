8023605693df4526fb94f3eedddda80a4c306d74
Peter Dijkstra <peter@macbook-van-peter-dijkstra-4.local>
2011-10-11 21:15:11 +0200

    random_forest.js add

---
+++ assets/maps/random_forest.js
@@ -0,0 +1,82 @@
+var maps = {}
+maps.getTiles= function() {
+   _tiles=[];
+   for(x=0;x<100;x+=1) {
+      for(y=0;y<100;y+=1) {
+        
+        floorId=parseInt(Math.random()*3);
+        if(floorId>1) {
+            itemid=2;
+            _tiles.push({
+              "_yindex": y,
+              "_xindex": x,
+              "_floorid": floorId,
+              "_itemid":itemid
+              });
+        } else {
+          _tiles.push({
+            "_yindex": y,
+            "_xindex": x,
+            "_floorid": floorId
+            });
+        }
+        
+        
+         
+      }   
+  }
+  return _tiles;
+}
+maps.forest = {
+	"image":"assets/maps/graphics/simpleGarden.png",
+	"tileWidth":32,
+	"rows":100,
+	"cols":100,
+    "enterTiles": [],
+    "slices": [{
+        "_h": 16,
+        "_w": 32,
+        "_y": 0,
+        "_bmp": null,
+        "_oy": 0,
+        "_id": 0,
+        "_x": 0,
+        "_ox": 0,
+        "_type": 0
+    },
+    {
+        "_h": 16,
+        "_w": 32,
+        "_y": 17,
+        "_bmp": null,
+        "_oy": 0,
+        "_id": 1,
+        "_x": 0,
+        "_ox": 0,
+        "_type": 0
+    },
+    {
+        "_h": 16,
+        "_w": 32,
+        "_y": 34,
+        "_bmp": null,
+        "_oy": 0,
+        "_id": 2,
+        "_x": 0,
+        "_ox": 0,
+        "_type": 0
+    },
+    {
+        "_h": 16,
+        "_w": 32,
+        "_y": 0,
+        "_bmp": null,
+        "_oy": 0,
+        "_id": 3,
+        "_x": 34,
+        "_ox": 0,
+        "_type": 0
+    }],
+    "actions": [],
+    "tiles": maps.getTiles()
+}
